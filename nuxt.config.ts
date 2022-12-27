ae7d04cfd601af6c1078130de26bab9bc282a40f
Patrick Raedler <patoraedler@gmail.com>
2022-12-27 17:07:23 +0100

    chore: Init project

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [['@unocss/nuxt'], ['@vueuse/nuxt']],
+  css: ['@unocss/reset/tailwind.css'],
+})
+
