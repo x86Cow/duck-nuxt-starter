d1353d2f77c3651228a4277f9cdb2cfb4feb4a0c
Patrick Raedler <patoraedler@gmail.com>
2023-01-01 22:20:50 +0100

    chore: Create initial placeholder
    The initial placeholder content for the project has been created.

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <DuckWelcome />
   </div>
 </template>
---
+++ components/DuckWelcome.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref("This is a welcome message!")
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
